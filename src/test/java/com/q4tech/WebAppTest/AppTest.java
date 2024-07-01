c793885277893761a88f09653f0b4808fb22fe54
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-07-01 17:23:54 -0300

    first commit

---
+++ src/test/java/com/q4tech/WebAppTest/AppTest.java
@@ -0,0 +1,19 @@
+package com.q4tech.WebAppTest;
+
+import static org.testng.AssertJUnit.assertTrue;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

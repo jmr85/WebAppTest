80a6fbcba0ebfb2e93071d09e2a2d7691a22bc60
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-07-17 12:18:47 -0300

    optional parameter and gitignore vsc

--- src/test/java/com/q4tech/WebAppTest/LoginTestCrossBrowser.java
+++ src/test/java/com/q4tech/WebAppTest/LoginTestCrossBrowser.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -16,9 +17,10 @@ public class LoginTestCrossBrowser {
 	String url = "http://capital.q4tech.com:7272/sfNetWebApp.Web_acmeus/";
 	WebDriver driver;
 	
+	//el @Optional("Chrome") ejecuta test con Chrome si no le pasamos param desde testng.xml
 	@BeforeTest
 	@Parameters("browser")
-	public void setup(String navegador) {
+	public void setup(@Optional("Chrome") String navegador) {
 		if (navegador.equalsIgnoreCase("Chrome")) {
 			// Acciones para abrir el navegador Chrome	
 			ChromeOptions options = new ChromeOptions();
